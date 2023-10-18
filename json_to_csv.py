240aad2776f6a7e542c3fb5ff000ff1c75fa3d48
emdeh <72545829+emdeh@users.noreply.github.com>
2023-10-18 14:44:22 +1100

    new sheet of unique links

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,15 +1,7 @@
-'''
-Created this script because I was having trouble with the output of https://github.com/raviqqe/muffet (the Windows version specifically).
-This script takes a JSON output file from muffet and converts it into CSV.
-example:
-  Open a cmd line
-  Run muffet as you need like 'muffet.exe https://www.mysite.come /follow-robots-txt /max-connections:200 /format:json /v > results.json
-  Wait for it to finish
-  Then, in the same dir, run: json_to_csv.py results.json
-'''
 import sys
 import json
 import csv
+from collections import defaultdict
 
 if len(sys.argv) != 2:
     print("Usage: python json_to_csv.py <input_json_file>")
@@ -22,9 +14,12 @@ try:
         data = json.load(json_file)
 
     csv_file = input_json_file.replace('.json', '_converted.csv')
+    unique_csv_file = input_json_file.replace('.json', '_unique_converted.csv')
+
+    unique_links = {}
+    header = set(['Page'])
 
     with open(csv_file, 'w', newline='') as csv_file:
-        header = set(['Page'])  # Initialize the set with the new 'Page' field
         for item in data:
             for link in item.get('links', []):
                 properties = link.keys()
@@ -36,17 +31,30 @@ try:
         csv_writer.writeheader()
 
         for item in data:
-            top_level_url = item.get('url', 'Unknown')  # Get the top-level URL
+            top_level_url = item.get('url', 'Unknown')
             for link in item.get('links', []):
                 if not link.get('status'):
                     link['status'] = 'no status - check error column for more info'
-                
-                # Assign the top-level URL to the 'Page' field
+
                 link['Page'] = top_level_url
                 
                 csv_writer.writerow(link)
+                
+                # Store unique links
+                unique_links[link['url']] = link
 
-    print(f"Conversion completed. CSV file saved as {csv_file}")
+    # Create unique links CSV
+    with open(unique_csv_file, 'w', newline='') as unique_csv:
+        unique_column_order = [col for col in column_order if col != 'Page']  # Remove 'Page' from unique CSV
+        csv_writer = csv.DictWriter(unique_csv, fieldnames=unique_column_order)
+        csv_writer.writeheader()
+        
+        for unique_link in unique_links.values():
+            # Remove the 'Page' key-value pair before writing to unique CSV
+            unique_link.pop('Page', None)
+            csv_writer.writerow(unique_link)
+
+    print(f"Conversion completed. CSV files saved as {csv_file} and {unique_csv_file}")
 
 except FileNotFoundError:
     print(f"Error: The specified input JSON file '{input_json_file}' was not found.")
